6d5f08c489e92dcd2c2234d4959564252cebfc32
Dalya <dalyatobescu@gmail.com>
2024-12-03 19:39:04 +0200

    correct tests

--- test_main.py
+++ test_main.py
@@ -2,6 +2,6 @@ import pytest
 from main import add
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
     assert add(0, 0) == 0
-    assert add(-1, 1) == 0
+    assert add(-1, 1) == 1
