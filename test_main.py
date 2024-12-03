f89cee716f85856a4af19b56662bb9d1ec39a52a
Buracu Stefan <stefan_paul.buracu@stud.acs.upb.ro>
2024-12-03 23:50:56 +0200

    mai multe teste

--- test_main.py
+++ test_main.py
@@ -1,8 +1,14 @@
 import pytest
 from main import add
 
-def test_add():
+def test_add_positive_numbers():
     assert add(2, 3) == 5
+
+def test_add_zeros():
     assert add(0, 0) == 0
+
+def test_add_negative_and_positive():
     assert add(-1, 1) == 0
+
+def test_add_other_positive_numbers():
     assert add(3, 4) == 7
